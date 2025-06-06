c991662a24f80a0c861beb16ede2f589c2b0c898
VITOR PEDROSO <vitorsamuel283@gmail.com>
2025-06-05 21:30:05 -0300

    chore(routes): update controller references to match new Api namespace

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ZipCodeController;
+use App\Enums\TokenAbility;
+use App\Http\Controllers\Api\EmployeeController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ZipCodeController;
 use Illuminate\Support\Facades\Route;
 
 // Rotas públicas
@@ -13,4 +15,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', fn() => request()->user());
 
     Route::get('/zipcode/{cep}', [ZipCodeController::class, 'lookup']);
+
+    // Grupo de rotas administrativas para gerenciamento de usuários
+    Route::prefix('/admin/users')->group(function () {
+        Route::post('/', [EmployeeController::class, 'store'])
+            ->middleware('ability:manage-employees');
+    })->middleware('ability:' . TokenAbility::MANAGE_EMPLOYEES->value);;
 });
