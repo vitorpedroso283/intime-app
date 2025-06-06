f7bbb3f771221ec4970ec9aa61e50f21b1912e78
VITOR PEDROSO <vitorsamuel283@gmail.com>
2025-06-05 21:26:19 -0300

    refactor(api): move controllers to Api namespace for better organization

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Resources\LoginResource;
 use App\Services\AuthService;
--- app/Http/Controllers/Api/ZipCodeController.php
+++ app/Http/Controllers/Api/ZipCodeController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\ZipCodeLookupRequest;
 use App\Http\Resources\ZipCodeResource;
 use App\Services\ZipCodeService;
