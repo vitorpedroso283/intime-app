a3f45df4426b237ffaf48a0e4ec1f6314ea15944
VITOR PEDROSO <vitorsamuel283@gmail.com>
2025-06-05 21:01:12 -0300

    feat(employee): create CRUD controller and add required address fields to User model and SofDelete Trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +23,18 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'cpf',
+        'role',
+        'position',
+        'birth_date',
+        'zipcode',
+        'street',
+        'neighborhood',
+        'city',
+        'state',
+        'number',
+        'complement',
+        'created_by',
     ];
 
     /**
